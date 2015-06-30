afaea4fbdca867528963e9074e5e6d7125e3975e
Mollie Stephenson <mollie.c.stephenson@gmail.com>
2015-06-30 08:58:13 +0100

    list not working

---
+++ www/js/app.js
@@ -0,0 +1,28 @@
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var starter = angular.module('starter', ['ionic'])
+
+starter.controller('TodoCtrl', function() {
+  var self = this
+  self.tasks = [
+    { title: "Collect coins" },
+    { title: "Eat Mushrooms" },
+    { title: "Get high enough to grap the flag" },
+    { title: "Find the Princess" }
+  ];
+});
+
+starter.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
